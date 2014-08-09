182ffb3cc89c1461dc7e006ffb2733868dc1aef8
Ludovic <ltoinel@free.fr>
2014-08-10 00:39:48 +0200

    Adding Karotz

--- apps/multipush/config.js
+++ apps/multipush/config.js
@@ -11,7 +11,7 @@
 
 var config = {}
 
-// Zwaven
+// Multipush API
 config.version = '0.1';
 config.debug = false;
 
@@ -31,6 +31,14 @@ config.sms = {};
 config.sms.enabled = true;
 config.sms.url = 'http://192.168.1.3:9090/sendsms';
 config.sms.phone = "0646340951";
+
+// Karotz Alert
+config.karotz = {};
+config.karotz.enabled = true;
+config.karotz.installid = "0c21be47-30cd-48c2-9bed-8ec191b71205";
+config.karotz.apikey = "eae6399f-47cf-4199-9fb2-96b612f15521";
+config.karotz.secret = "8d3b3d3c-5f04-4e63-8ed9-a6f77769c1b2";
+config.karotz.language = "FR";
 
 module.exports = config;
 
