ff50d1cb17c4e6fe718fffd5c253987ff3fd36a5
Ludovic <ltoinel@free.fr>
2014-11-16 23:38:08 +0100

    Dynamic hour calculation for the shutters
    The hour to open and close the shutters is calculated by using the
    SunCalc library

--- apps/scheduler/tasks/shutters.js
+++ apps/scheduler/tasks/shutters.js
@@ -11,59 +11,83 @@
 
 var request = require('request');
 var CronJob = require('cron').CronJob;
+var SunCalc = require('suncalc');
 	
 //Local require
 var config = require('../config');
 
 // Local config
 config.shutters = {};
-config.shutters.enabled = false;
+config.shutters.enabled = true;
+config.shutters.latitude = 47.2;
+config.shutters.longitude = -1.5; 
 
 // Close
-config.shutters.close = {};
-config.shutters.close.hour = '00 30 21 * * *';
-config.shutters.close.url =  'http://192.168.1.4/down';
+config.shutters.url = {};
+config.shutters.url.close =  'http://192.168.1.4/xdown';
+config.shutters.url.open = 'http://192.168.1.4/xup';
 
-// Open
-config.shutters.open = {};
-config.shutters.open.hour = '00 30 9 * * *';
-config.shutters.open.url = 'http://192.168.1.4/up';
 
-/**
- * Open the shutters
- */ 
-var job = new CronJob(config.shutters.open.hour, function(){
+// Initialize a new job
+function initJob(name, uri, timeFunction, date){
+	var time = timeFunction(date);
+	console.log("Job initializing : " + name + " at " + time);
+	var job = new CronJob(time,function(){
+		
+		console.log("Job starting : " + name);
+		request.get(uri);
+		
+	},function(){
+		
+		console.log("Job completed : " + name);
+		
+		// Start the next job
+		initJob(name, uri, timeFunction, getTomorrowDate());
+		
+		// Stop the current job
+		this.stop();
+		
+	}, false, config.timezone).start();
+}
 
-	if (config.shutters.enabled){
-		console.log("Opening the shutters");
-		request.get(config.shutters.open.url);
-	};
+// Return the date of tomorrow
+function getTomorrowDate(){
+	var today = new Date();
+	var tomorrow = new Date();
+	tomorrow.setDate(today.getDate()+1);
+	return tomorrow;
+}
+
+// Return the sunrise time for the date in cron format
+var getSunriseTime = function(date){
 	
-  }, function () {
-		if (config.shutters.enabled){
-			console.log('Shutters are open');
-		};
-  },
-  false,
-  config.timezone 
-).start();
-
-
-/**
- * Close the shutters
- */
-var job = new CronJob(config.shutters.close.hour, function(){
 	
-	if (config.shutters.enabled){
-		console.log("Closing the shutters");
-		request.get(config.shutters.close.url);
-	};
+	var times = SunCalc.getTimes(date, config.shutters.latitude, config.shutters.longitude);
+	console.log("Get sunrise time for " + date + " => " + times.sunriseEnd);
 	
-  }, function () {
-		if (config.shutters.enabled){
-			console.log('Shutters are closed');
-		};
-  },
-  false,
-  config.timezone 
-).start();
+	if (new Date() > times.sunriseEnd){ 
+		console.log("Deprecated Sunrise time, using tomorow time");
+		times = SunCalc.getTimes(getTomorrowDate(), config.shutters.latitude, config.shutters.longitude);
+	}
+	
+	//return times.sunriseEnd.getSeconds() +" "+times.sunriseEnd.getMinutes()+" "+times.sunriseEnd.getHours()+" * * *";
+	return "0 31 23 * * *";
+}
+
+// Return the sunset time for the date in cron format
+var getSunsetTime = function(date){
+	
+	var times = SunCalc.getTimes(date, config.shutters.latitude, config.shutters.longitude);
+	console.log("Get sunset time for " + date + " => " + times.sunset);
+	
+	if (new Date() > times.sunset){ 
+		console.log("Deprecated Sunset time, using tomorow time");
+		times = SunCalc.getTimes(getTomorrowDate(), config.shutters.latitude, config.shutters.longitude);
+	}
+	return times.sunset.getSeconds() +" "+times.sunset.getMinutes()+" "+times.sunset.getHours()+" * * *";
+}
+
+// Initialize
+initJob("Ouverture automatique des volets", config.shutters.url.open, getSunriseTime, new Date());
+initJob("Fermeture automatique des volets", config.shutters.url.close, getSunsetTime, new Date());
+
